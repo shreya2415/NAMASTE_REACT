e9b3b23deb039143396616d44526f28b9fe3b184
SHREYA AGRAWAL <agrawalShreya2525@gmail.com>
2024-03-14 18:28:13 +0530

    epi-1

---
+++ App.js
@@ -0,0 +1,22 @@
+const heading = React.createElement("h1", {
+    id: "heading",
+    xyx: "abc"
+}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
+// <div id ="parent">
+//     <div id ="child">
+//         <h1></h1>
+//     </div>
+// </div>
+
+// const parent = React.createElement("div", {
+//     id: "parent"
+// }, React.createElement("div", {
+//     id: "child"
+// }, React.createElement("h1", {}, "I am react!")));
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent); 
