8d22b000a840814b1328a4faec1af09edc369e72
Shreya Agrawal <agrawalshreya2515@gmail.com>
2024-03-14 18:36:33 +0530

    New Changes

--- App.js
+++ App.js
@@ -2,21 +2,21 @@ const heading = React.createElement("h1", {
     id: "heading",
     xyx: "abc"
 }, "Hello World from React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
 
-// <div id ="parent">
-//     <div id ="child">
-//         <h1></h1>
-//     </div>
-// </div>
+<div id ="parent">
+    <div id ="child">
+        <h1></h1>
+    </div>
+</div>
 
-// const parent = React.createElement("div", {
-//     id: "parent"
-// }, React.createElement("div", {
-//     id: "child"
-// }, React.createElement("h1", {}, "I am react!")));
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, React.createElement("h1", {}, "I am react!")));
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent); 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); 
