0d67530ddfe6b8270d31b60f6b495ee8e5dd001e
Shreya Agrawal <agrawalshreya2515@gmail.com>
2024-03-15 18:45:56 +0530

    Epi2

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {
     id: "heading",
     xyx: "abc"
 }, "Hello World from React!");
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
-
-<div id ="parent">
-    <div id ="child">
-        <h1></h1>
-    </div>
-</div>
+  
+/* <div id ="parent"></div>
 
 const parent = React.createElement("div", {
     id: "parent"
 }, React.createElement("div", {
     id: "child"
 }, React.createElement("h1", {}, "I am react!")));
+*/
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); 
+////const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(parent);  
--- Readme.md
+++ Readme.md
@@ -1,4 +1,20 @@
 * NPM does not stand for only Node package manager , it is a manager of package not specifically for node. 
 
+* NPM means installing a package
+* NPX means executing a package.
 
+* CDN links are not generic and preferred method to use react inside in our code.
 
+# Parcel Functionality
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement 
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimisation
+- Minification of files
+- Bundling the file
+- Compress the file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support Older Browser
