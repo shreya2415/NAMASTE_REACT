03cc067c8ed4de8ce1584c48ed3765419d88f4f8
Shreya Agrawal <agrawalshreya2515@gmail.com>
2024-03-17 20:36:08 +0530

    Food Ordering

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
+/** React Food Ordering app Structure
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ * - Search
+ * - RestrauntContainer
+ *  - RestruantCard
+ *  -- Img
+ *  -- Name of Res, Rating, Cuisine , deliveryTime
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+const Header =() =>{
+    return(
+        <div className = "header">
+            <div className="logo">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
 
-// const heading = React.createElement(
-//     "h1",
-//     {id : "heading"},
-//     "Namaste React :)"
-// );
+    )
+};
+const RestruantCard = () =>{
+    return (
+   <div className="res-card">
+            <h1> Megana Foods</h1>
+   </div>
+    )
+};
+const Body =() => {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+               <RestruantCard />
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-//Using JSX
-//JSX is camelCase
-const jsxHeading = <h1 className="head">Namaste Using JSX</h1>
+            </div>
+        </div>
+    )
+ };
+ const AppLayout =() =>{
+    return (
+        <div className = "app">
+                 <Header />
+                 <Body />
+        </div>
+   
+    )
+ }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+ root.render(<AppLayout />);
